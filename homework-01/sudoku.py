821c60acb60840e11e72c5eac5bb45106ecc1f61
Зайцев Никита <zajcevnikita@MacBook-Air-Zajcev.local>
2024-02-09 13:16:09 +0300

    group changed

--- homework-01/sudoku.py
+++ homework-01/sudoku.py
@@ -42,7 +42,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    print('')
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
